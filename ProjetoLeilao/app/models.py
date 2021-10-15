67b9b2fe493f307be33c278f539b0c717c369761
Silas Lima e Silva <silaslima@usp.br>
2021-10-15 18:36:15 -0300

    inicio login

--- ProjetoLeilao/app/models.py
+++ ProjetoLeilao/app/models.py
@@ -1,12 +1,16 @@
 from django.db import models
-
+from django.contrib.auth.models import AbstractUser
 from django.urls import reverse
+
+class User(AbstractUser):
+    is_client = models.BooleanField(default = False)
+    is_admin = models.BooleanField(default = False)
 
 class Comprador(models.Model):
     nome = models.CharField(max_length = 200)
     cpf = models.DecimalField(max_digits=11, decimal_places=0)
     bio = models.TextField()
-
+    user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
     def __str__(self):
         return self.nome
 
