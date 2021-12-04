e57dcb3eee33fd2359ca510fd04265bb07d357f1
ArkhamKnightGPC <nazter57@usp.br>
2021-12-04 08:42:33 -0300

    :bricks: tutorial okteto

---
+++ run_web.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+# prepare init migration
+
+python manage.py makemigrations myproject
+echo "Created migrations"
+
+# migrate db, so we have the latest db schema
+
+python manage.py migrate
+echo "Migrated DB to latest version"
+
+#start server
+
+echo "Starting server"
+gunicorn ProjetoLeilao.wsgi
