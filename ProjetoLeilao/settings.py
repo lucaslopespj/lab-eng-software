94b1d479628ba4dc5fab93f0ee08f615c0f5fc70
ArkhamKnightGPC <nazter57@usp.br>
2021-12-04 13:24:49 -0300

    :pencil2: deploy com BD para heroku

--- ProjetoLeilao/settings.py
+++ ProjetoLeilao/settings.py
@@ -1,5 +1,4 @@
 from pathlib import Path
-import dj_database_url
 
 import os
 
@@ -69,12 +68,10 @@ WSGI_APPLICATION = 'ProjetoLeilao.wsgi.application'
 
 DATABASES = {
     'default': {
-        #'ENGINE': 'django.db.backends.sqlite3',
-        #'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-db_from_env = dj_database_url.config(conn_max_age=600)
-DATABASES['default'].update(db_from_env)
 
 #DATABASES = {
 #    'default': {
